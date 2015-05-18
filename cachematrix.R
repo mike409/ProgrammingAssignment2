481c9aa71fc21c7f62a18be3d8797f8d95f3d22c
mike409 <michaeltmartin@yahoo.com>
2015-05-18 10:03:52 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Coursera R Progr  Assignment 2
+## using cache 
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## makeCacheMatrix contains functions used to set and retrieve cache 
+    m<-NULL
+    set<-function(y){
+         ## reset the super assignments
+         x<<-y
+         m<<-NULL
+     }
+    get<-function() x
+    setmatrix<-function(solve) m<<- solve
+    getmatrix<-function() m
+    list(set=set, get=get,
+         setmatrix=setmatrix,
+         getmatrix=getmatrix)
+} 
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ cacheSolve <- function(x, ...) { 
+  ## Return a matrix that is the inverse of the matrix 'x' 
+  ## we assume the matrix has an inverse
+     m<-x$getmatrix()
+     if(!is.null(m)){
+        message("getting cached data")
+     }
+     else {
+        matrx<-x$get()
+        m<-solve(matrx, ...)
+        x$setmatrix(m)
+     } 
+     m
 }
