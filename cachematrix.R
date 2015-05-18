31fccd85829bae456c9c4ba2a6138c438975a5dc
mike409 <michaeltmartin@yahoo.com>
2015-05-18 15:45:58 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,32 +2,44 @@
 ## using cache 
 
 makeCacheMatrix <- function(x = matrix()) {
-    ## makeCacheMatrix contains functions used to set and retrieve cache 
-    m<-NULL
-    set<-function(y){
-         ## reset the super assignments
-         x<<-y
-         m<<-NULL
-     }
-    get<-function() x
-    setmatrix<-function(solve) m<<- solve
-    getmatrix<-function() m
-    list(set=set, get=get,
-         setmatrix=setmatrix,
-         getmatrix=getmatrix)
+  ## makeCacheMatrix contains functions used to set and retrieve cache 
+  inv_mat<-NULL
+
+  set<-function(y){
+    ## reset the super assignments
+    ## the matix is reset to y
+    ## the mean (m) is reset to NULL
+    x<<-y
+    inv_mat<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) inv_mat <<- solve
+  getmatrix<-function() inv_mat
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 } 
 
- cacheSolve <- function(x, ...) { 
+cacheSolve <- function(x, ...) { 
   ## Return a matrix that is the inverse of the matrix 'x' 
   ## we assume the matrix has an inverse
-     m<-x$getmatrix()
-     if(!is.null(m)){
-        message("getting cached data")
-     }
-     else {
-        matrx<-x$get()
-        m<-solve(matrx, ...)
-        x$setmatrix(m)
-     } 
-     m
+  
+  ## inv_mat <- the inverse of x 
+  inv_mat<-x$getmatrix()
+  if(!is.null(inv_mat)){
+    message("getting cached data")
+    ##print(m)
+    ##te<-solve(m)
+    ##message("Inv matrix below")
+    ##print(te)
+    ##message("Orig %*% Inv matrix below")
+    ##print(m %*% te)
+    ##message("Orig matrix below")
+    inv_mat
+  }
+  
+    matr<-x$get()
+    inv_mat<-solve(matr, ...)
+    x$setmatrix(inv_mat)
+    inv_mat
 }
